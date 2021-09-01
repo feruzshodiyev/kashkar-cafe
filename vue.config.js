f04fe75429edf4e30bd7a10e746b0d4078ec0b0f
feruzshodiyev <feruzshodiyev1009@mail.ru>
2021-09-01 16:18:47 +0500

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   devServer: {
     port: 3000,
     // proxy: "https://kashkarapi.pythonanywhere.com"
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/kashkar-cafe/'
+        : '/'
   },
 };
